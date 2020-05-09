e1582a5ff38e46a5de3f71f3bc388be4d6efef9f
Prasanna Vengateshan Varadharajan <Prasanna.VengateshanVaradharajan@microchip.com>
2020-05-09 16:44:19 +0530

    Adding missing files & compiled version

--- src/Atmel_SOC_SAMA5D3/buildroot-at91/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
+++ src/Atmel_SOC_SAMA5D3/buildroot-at91/board/atmel/sama5d3_xplained_ksz9897/post-image.sh
@@ -2,5 +2,5 @@
 
 DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
 
-cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
+#cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
 
