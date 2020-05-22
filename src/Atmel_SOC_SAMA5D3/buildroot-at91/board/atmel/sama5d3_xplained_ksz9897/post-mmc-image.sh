957795738a2df2cc71fdfa333aa36edd3dd7ee3c
root <root@CHE-LT-I15255.mchp-main.com>
2020-05-22 11:16:51 +0530

    Added files to support SD boot

--- src/Atmel_SOC_SAMA5D3/buildroot-at91/board/atmel/sama5d3_xplained_ksz9897/post-mmc-image.sh
+++ src/Atmel_SOC_SAMA5D3/buildroot-at91/board/atmel/sama5d3_xplained_ksz9897/post-mmc-image.sh
@@ -4,9 +4,11 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
+#commenting out these 2 lines as they are just for finding and renaming the .dtb file
+#DTB=$(grep BR2_LINUX_KERNEL_INTREE_DTS_NAME $BR2_CONFIG | cut -d"\"" -f2)
 
-cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
+#cp -p ${BINARIES_DIR}/$DTB.dtb ${BINARIES_DIR}/at91-sama5d3_xplained.dtb
+
 install -p -m 644 ${BOARD_DIR}/uboot.env ${BINARIES_DIR}/uboot.env
 
 rm -rf "${GENIMAGE_TMP}"
@@ -16,4 +18,5 @@ genimage                               \
 	--tmppath "${GENIMAGE_TMP}"    \
 	--inputpath "${BINARIES_DIR}"  \
 	--outputpath "${BINARIES_DIR}" \
-	--config "${GENIMAGE_CFG}"
+	--config "${GENIMAGE_CFG}"		\
+	--loglevel 5
