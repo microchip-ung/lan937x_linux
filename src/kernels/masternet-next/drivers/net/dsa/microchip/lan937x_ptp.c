400d5de71ee55c8e3761c19cb2365699007f92de
arun <arun.ramadoss@microchip.com>
2020-10-21 20:03:42 +0530

    working set get hwstamp

--- src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_ptp.c
+++ src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_ptp.c
@@ -2,7 +2,9 @@
 #include "ksz_common.h"
 #include "lan937x_ptp.h"
 
-
+/* The function is return back the capability of timestamping feature when requested
+   through ethtool -T <interface> utility
+   */
 int lan937x_get_ts_info(struct dsa_switch *ds, int port, struct ethtool_ts_info *ts)
 {
 
@@ -15,7 +17,7 @@ int lan937x_get_ts_info(struct dsa_switch *ds, int port, struct ethtool_ts_info 
 	ts->tx_types = (1 << HWTSTAMP_TX_OFF) |
 		       (1 << HWTSTAMP_TX_ON);
 
-	ts->rx_filters = (1 << HWTSTAMP_FILTER_NONE) |
+	ts->rx_filters = (1 << HWTSTAMP_FILTER_NONE)  |
 		         (1 << HWTSTAMP_FILTER_PTP_V2_L2_EVENT);
 
 	return 0;
@@ -24,13 +26,28 @@ int lan937x_get_ts_info(struct dsa_switch *ds, int port, struct ethtool_ts_info 
 
 int lan937x_hwtstamp_get(struct dsa_switch *ds, int port, struct ifreq *ifr)
 {
+	struct ksz_device *dev  = ds->priv;
+	struct hwtstamp_config *port_tconfig = &dev->ports[port].tstamp_config;
 
 
-	return 0;
+	return copy_to_user(ifr->ifr_data, port_tconfig, sizeof(struct hwtstamp_config)) ?  
+	        -EFAULT : 0;
 }
 
 int lan937x_hwtstamp_set(struct dsa_switch *ds, int port, struct ifreq *ifr)
 {
+	struct ksz_device *dev  = ds->priv;
+	struct hwtstamp_config *port_tconfig = &dev->ports[port].tstamp_config;
+	struct hwtstamp_config config;
 
-	return 0;
+	if(copy_from_user(&config, ifr->ifr_data, sizeof(config)))
+		return -EFAULT;
+	
+	//save the configuration in the ksz_port
+	memcpy(port_tconfig, &config, sizeof(config));
+	
+
+	return copy_to_user(ifr->ifr_data, &config, sizeof(config)) ?  
+	        -EFAULT : 0;
 }
+
