6b676461ad8e0702f751a213e13cc0a2f45e1ec2
arun <arun.ramadoss@microchip.com>
2020-10-22 14:35:07 +0530

    conditional compilation using kconfig

--- src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_ptp.h
+++ src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_ptp.h
@@ -1,8 +1,27 @@
 #ifndef NET_DSA_DRIVERS_LAN937X_PTP_H
 #define NET_DSA_DRIVERS_LAN937X_PTP_H
 
+#if IS_ENABLED(CONFIG_NET_DSA_MICROCHIP_LAN937X_PTP)
 int lan937x_get_ts_info(struct dsa_switch *ds, int port, struct ethtool_ts_info *ts);
 int lan937x_hwtstamp_get(struct dsa_switch *ds, int port, struct ifreq *ifr);
 int lan937x_hwtstamp_set(struct dsa_switch *ds, int port, struct ifreq *ifr);
+#else
+
+static inline int lan937x_get_ts_info(struct dsa_switch *ds, int port, struct ethtool_ts_info *ts)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int lan937x_hwtstamp_get(struct dsa_switch *ds, int port, struct ifreq *ifr)
+{
+	return -EOPNOTSUPP;
+}
+
+static inline int lan937x_hwtstamp_set(struct dsa_switch *ds, int port, struct ifreq *ifr)
+{
+	return -EOPNOTSUPP;
+}
+
+#endif  /* End of CONFIG_NET_DSA_MICROCHIOP_LAN937X_PTP */
 
 #endif
