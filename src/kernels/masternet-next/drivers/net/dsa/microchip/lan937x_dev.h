6332676f284b5f7d3bbbb70f48cf891fd8c6e5d0
Prasanna Vengateshan <prasanna.vengateshan@microchip.com>
2020-11-26 18:47:52 +0530

    lan937x_dev.h header file optimizations

--- src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_dev.h
+++ src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_dev.h
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: GPL-2.0
-/* Microchip 
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Microchip lan937x dev ops headers
  * Copyright (C) 2019-2020 Microchip Technology Inc.
  */
 
@@ -8,36 +8,36 @@
 
 void lan937x_cfg(struct ksz_device *dev, u32 addr, u8 bits, bool set);
 void lan937x_port_cfg(struct ksz_device *dev, int port, int offset,
-			     u8 bits, bool set);
+		      u8 bits, bool set);
 void lan937x_cfg32(struct ksz_device *dev, u32 addr, u32 bits, bool set);
 void lan937x_pread8(struct ksz_device *dev, int port, int offset,
-				  u8 *data);
+		    u8 *data);
 void lan937x_pread16(struct ksz_device *dev, int port, int offset,
-				   u16 *data);
+		     u16 *data);
 void lan937x_pread32(struct ksz_device *dev, int port, int offset,
-				   u32 *data);
+		     u32 *data);
 void lan937x_pwrite8(struct ksz_device *dev, int port,
-				   int offset, u8 data);
+		     int offset, u8 data);
 void lan937x_pwrite16(struct ksz_device *dev, int port,
-				    int offset, u16 data);
+		      int offset, u16 data);
 void lan937x_pwrite32(struct ksz_device *dev, int port,
-				    int offset, u32 data);
+		      int offset, u32 data);
 void lan937x_port_cfg32(struct ksz_device *dev, int port, int offset,
-			       u32 bits, bool set);     
+			u32 bits, bool set);
 int lan937x_t1_tx_phy_write(struct ksz_device *dev, int addr,
-				   int reg, u16 val);
+			    int reg, u16 val);
 int lan937x_t1_tx_phy_read(struct ksz_device *dev, int addr,
-				  int reg, u16 *val);
-bool lan937x_is_internal_tx_phy_port (struct ksz_device *dev, int port);
-bool lan937x_is_internal_phy_port (struct ksz_device *dev, int port);
+			   int reg, u16 *val);
+bool lan937x_is_internal_tx_phy_port(struct ksz_device *dev, int port);
+bool lan937x_is_internal_phy_port(struct ksz_device *dev, int port);
 int lan937x_reset_switch(struct ksz_device *dev);
 void lan937x_cfg_port_member(struct ksz_device *dev, int port,
-				    u8 member);
+			     u8 member);
 void lan937x_port_setup(struct ksz_device *dev, int port, bool cpu_port);
 int lan937x_sw_register(struct ksz_device *dev);
 int lan937x_enable_spi_indirect_access(struct ksz_device *dev);
 
-struct mib_names{
+struct mib_names {
 	int index;
 	char string[ETH_GSTRING_LEN];
 };
