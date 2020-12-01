704d82176d1e0cc2d551eafd07777780c238c1cb
Prasanna Vengateshan <prasanna.vengateshan@microchip.com>
2020-12-01 16:33:55 +0530

    One header file inclusion not required

--- src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_main.c
+++ src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_main.c
@@ -5,7 +5,6 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/iopoll.h>
-#include <linux/platform_data/microchip-ksz.h>
 #include <linux/phy.h>
 #include <linux/if_bridge.h>
 #include <net/dsa.h>
