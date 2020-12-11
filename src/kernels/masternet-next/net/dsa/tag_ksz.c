94ae1e195981d2502e4bafa08bd089193c8b89a5
arun <arun.ramadoss@microchip.com>
2020-12-11 21:20:44 +0530

    added the flag for sending 4 bytes tail depends on ioctl

--- src/kernels/masternet-next/net/dsa/tag_ksz.c
+++ src/kernels/masternet-next/net/dsa/tag_ksz.c
@@ -391,6 +391,8 @@ static struct sk_buff *lan937x_xmit(struct sk_buff *skb,
 {
 	struct dsa_port *dp = dsa_slave_to_port(dev);
 	struct lan937x_port_ext *kp = dp->priv;
+	struct dsa_switch *ds = dev->dsa_ptr->ds;
+	struct ksz_device *ksz = ds->priv;
 	struct sk_buff *nskb;
 	__be16 *tag;
 	u8 *addr;
@@ -402,7 +404,9 @@ static struct sk_buff *lan937x_xmit(struct sk_buff *skb,
 		return NULL;
 
 	/* Tag encoding */
-        lan937x_xmit_timestamp(skb);
+	if (test_bit(LAN937X_HWTS_EN, &ksz->ptp_shared.state))
+        	lan937x_xmit_timestamp(skb);
+
 	tag = skb_put(nskb, LAN937X_INGRESS_TAG_LEN);
 	addr = skb_mac_header(nskb);
 
