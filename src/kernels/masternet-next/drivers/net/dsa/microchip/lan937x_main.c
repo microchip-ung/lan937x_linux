7bd0534c4ac212737189401886b3d04748b0667e
arun <arun.ramadoss@microchip.com>
2021-01-21 14:34:05 +0530

    added the spi access

--- src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_main.c
+++ src/kernels/masternet-next/drivers/net/dsa/microchip/lan937x_main.c
@@ -922,6 +922,9 @@ static int lan937x_setup(struct dsa_switch *ds)
 	/* enable global MIB counter freeze function */
 	lan937x_cfg(dev, REG_SW_MAC_CTRL_6, SW_MIB_COUNTER_FREEZE, true);
 
+        /* enable Indirect Access from SPI to the VPHY registers */
+	lan937x_enable_spi_indirect_access(dev);
+
 	ret = lan937x_ptp_init(ds);
 	if (ret < 0) {
 		dev_err(ds->dev, "Failed to register PTP clock: %d\n", ret);
