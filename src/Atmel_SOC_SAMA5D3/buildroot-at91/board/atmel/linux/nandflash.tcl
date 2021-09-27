b951d8158ef06bf750f58849fea512256b9677e7
arun <arun.ramadoss@microchip.com>
2021-09-27 18:45:06 +0530

    reverted the nandflash.tcl to working state

--- src/Atmel_SOC_SAMA5D3/buildroot-at91/board/atmel/linux/nandflash.tcl
+++ src/Atmel_SOC_SAMA5D3/buildroot-at91/board/atmel/linux/nandflash.tcl
@@ -293,10 +293,10 @@ if {! [file exists $rootfsFile]} {
 ## NandFlash Mapping
 set bootStrapAddr	0x00000000
 set ubootAddr		0x00040000
-set ubootEnvAddr	0x00140000
-set dtbAddr		0x001c0000
-set kernelAddr		0x00240000
-set rootfsAddr		0x00840000
+set ubootEnvAddr	0x00100000
+set dtbAddr		0x00180000
+set kernelAddr		0x00200000
+set rootfsAddr		0x00800000
 
 ## u-boot variable
 set kernelLoadAddr [get_kernel_load_addr $boardFamily]
@@ -308,10 +308,6 @@ set dtbSize	[format "0x%08X" [expr (([file size $dtbFile] + 1023) / 1024) * 1024
 set loadDts "load_dts=nand read $dtbLoadAddr $dtbAddr $dtbSize; bootz $kernelLoadAddr - $dtbLoadAddr"
 set bootCmd "bootcmd=nand read $kernelLoadAddr $kernelAddr $kernelSize; run prep_boot; run load_dts"
 set rootfsSize	[format "0x%08X" [file size $rootfsFile]]
-#set bootCmd "bootcmd=run nfsboot;"
-#set rootpath "192.43.1.1:/home/administrator/linux_dd/rootfs"
-#set ip_full "192.43.1.9:192.43.1.1::255.255.255.0:root:lan1:off"
-    #"bootargs=console=ttyS0,115200 root=/dev/nfs nfsroot=${rootpath},nolock,v3,tcp ip=${ip_full} rw"\
 
 lappend u_boot_variables \
     "bootdelay=1" \
@@ -322,12 +318,11 @@ lappend u_boot_variables \
     "bootargs=console=ttyS0,115200 mtdparts=atmel_nand:256k(bootstrap)ro,768k(uboot)ro,256k(env),256k(env_redundant),512k(dtb),6M(kernel)ro,-(rootfs) rootfstype=ubifs ubi.mtd=6 root=ubi0:rootfs rw $videoMode" \
     "_bootargs=console=ttyS0,115200 mtdparts=atmel_nand:256k(bootstrap)ro,768k(uboot)ro,256k(env),256k(env_redundant),512k(dtb),6M(kernel)ro,-(rootfs) rootfstype=ubifs ubi.mtd=6 root=ubi0:rootfs rw $videoMode" \
     "ethaddr=00:10:A1:93:74:10" \
-    "ipaddr=192.43.1.9" \
-    "serverip=192.43.1.1" \
+    "ipaddr=192.168.0.111" \
+    "serverip=192.168.0.100" \
     "subst_var=0" \
     "prep_boot=setenv -f subst_var 1; setenv -f bootargs \"\${_bootargs}\"" \
     "$loadDts" \
-    "nfsboot=tftp $kernelLoadAddr zImage; tftp $dtbLoadAddr at91-sama5d3_xplained.dtb; bootz $kernelLoadAddr - $dtbLoadAddr"\
     "$bootCmd"
 
 if {$program == "yes"} {
@@ -377,7 +372,7 @@ if {$program == "yes"} {
       NANDFLASH::NandSetTrimffs 1
    }
 
-   #puts "-I- === Load the linux file system ==="
+   puts "-I- === Load the linux file system ==="
    send_file {NandFlash} "$rootfsFile" $rootfsAddr 0
 
    puts "-I- === DONE. ==="
