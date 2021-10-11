ef115e9ada487b82042ee0874f76cd9d497bfeee
Arun Ramadoss <arun.ramadoss@microchip.com>
2021-10-11 12:22:30 +0530

    [TL-69] addressed the community feedback

--- src/kernels/masternet-next/drivers/net/dsa/microchip/ksz_common.c
+++ src/kernels/masternet-next/drivers/net/dsa/microchip/ksz_common.c
@@ -94,8 +94,7 @@ static void ksz_mib_read_work(struct work_struct *work)
 		mutex_unlock(&mib->cnt_mutex);
 	}
 
-        if(dev->mib_read_interval)
-                schedule_delayed_work(&dev->mib_read, dev->mib_read_interval);
+	schedule_delayed_work(&dev->mib_read, dev->mib_read_interval);
 }
 
 void ksz_init_mib_timer(struct ksz_device *dev)
@@ -457,9 +456,9 @@ void ksz_switch_remove(struct ksz_device *dev)
 {
 	/* timer started */
 	if (dev->mib_read_interval) {
-                dev->mib_read_interval = 0;
+		dev->mib_read_interval = 0;
 		cancel_delayed_work_sync(&dev->mib_read);
-        }
+	}
 
 	dev->dev_ops->exit(dev);
 	dsa_unregister_switch(dev->ds);
