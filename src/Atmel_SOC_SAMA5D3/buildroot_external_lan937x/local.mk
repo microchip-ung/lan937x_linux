d23b53014b1940fb47051bc178526d3493b249cb
Rakesh Sankaranarayanan <rakesh.sankaranarayanan@microchip.com>
2022-09-30 12:53:07 +0530

    comment fixes for dt-overlay

--- src/Atmel_SOC_SAMA5D3/buildroot_external_lan937x/local.mk
+++ src/Atmel_SOC_SAMA5D3/buildroot_external_lan937x/local.mk
@@ -1,2 +1,2 @@
-LINUX_OVERRIDE_SRCDIR = "/home/che-lt-i67786lx/Workspace/net-next"
+#LINUX_OVERRIDE_SRCDIR = "/home/che-lt-i67786lx/Workspace/net-next"
 DT_OVERLAY_MCHP_OVERRIDE_SRCDIR = "$(BR2_EXTERNAL_LAN937x_PATH)/package/dt-overlay-mchp/dtbos"
